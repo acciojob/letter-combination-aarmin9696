f674985ab1542e4019eb4b79dee89b2135999b49
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-15 12:26:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-solve(index,calculatedString,input_digit){
-	let mapping = {
-		"2":"abc",
-		"3":"def",
-		"4":"ghi",
-		"5":"jkl",
-		"6":"mno",
-		"7":"pqrs",
-		"8":"tuv",
-		"9":"wxyz"
-	}
-	let result = []
-	if(index>=input_digit.length){
-		result.push(calculatedString)
-		retun result
-	}
-	let currString = mapping[input_digit[index]]
-	for(let i=0;i<currString.length;i++){
-		solve(index+1,calculatedString+currString[i],input_digit)
-	}
+function letterCombinations(digits) {
+  // Mapping of digit to letters
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  // Function to generate letter combinations recursively
+  function generateCombinations(combination, digits) {
+    // Base case: if there are no more digits, add the current combination to the result
+    if (digits.length === 0) {
+      result.push(combination);
+      return;
+    }
+
+    // Get the letters corresponding to the first digit
+    const letters = digitToLetters[digits[0]];
+
+    // Recursively generate combinations for the remaining digits
+    for (let i = 0; i < letters.length; i++) {
+      generateCombinations(combination + letters[i], digits.substring(1));
+    }
+  }
+
+  const result = [];
+
+  // Call the helper function to generate letter combinations
+  generateCombinations('', digits);
+
+  // Sort the result lexicographically
+  result.sort();
+
+  return result;
 }
 function letterCombinations(input_digit) {
   //Complete the function
 	
-	solve(0,"",input_digit)
+	generateCombinations('', input_digit);
 	
 }
 
